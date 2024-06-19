978e72e1698abd06f78ab759a9a1dc35a688ed5b
Eric Liang <eric@bmxlabs.com>
2024-06-20 04:45:44 +0800

    update scripts for pic location

--- zh-hans/scripts/update-articles.sh
+++ zh-hans/scripts/update-articles.sh
@@ -2,3 +2,5 @@
 DIR="articles"
 
 cat SUMMARY.md | awk -F'/' '{if($3 != "" && substr($1, length($1)-7, 8) == "articles") print $1"/"$3; else print $0}' > SUMMARY-articles.md 
+
+find articles -name "*.md" -exec gsed -i 's/\.\.\/\.\.\//\.\.\//g' {} \;
